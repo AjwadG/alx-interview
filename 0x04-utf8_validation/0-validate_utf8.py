95f82ad77d1c3a8c52773ecaa4311c6b1c71ec82
AjwadG <ajwadelgandouz@yahoo.com>
2024-05-23 21:40:19 +0200

    0. UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+0-validate_utf8.py
+"""
+
+
+def binary(number):
+    '''
+        checks and returns the number of leading ones
+        if the number is not valid it returns -1
+    '''
+    for a in range(7, 2, -1):
+        if not number >> a & 1:
+            return 7 - a
+    return -1
+
+
+def validUTF8(data):
+    '''
+        determines if a given data set represents a valid UTF-8 encoding
+    '''
+    prev = 0
+    for number in data:
+        n_bytes = binary(number)
+        if not prev and n_bytes == 1:
+            return False
+        if prev == 0:
+            prev = n_bytes
+        elif prev and (n_bytes == 0 or n_bytes > 1) or n_bytes == -1:
+            return False
+        prev = prev - 1 if prev != 0 else prev
+
+    return prev == 0
